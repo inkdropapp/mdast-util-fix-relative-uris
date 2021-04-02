bfb98f0181de299077d50d30bf848f4f9d55b022
Takuya Matsuyama <hi@craftz.dog>
2021-04-02 11:48:27 +0900

    test(*): ignore anchor links

--- test/index.js
+++ test/index.js
@@ -27,3 +27,15 @@ test('absolute URL', (t) => {
   t.is(typeof result, 'string')
   t.is(result, '[link](https://www.inkdrop.app/)\n')
 })
+
+test('ignore anchor links', (t) => {
+  const md = `[link](#about)`
+  const mdast = fromMarkdown(md)
+  t.is(typeof mdast, 'object')
+  const resolved = fixRelativeURIs(mdast, 'https://www.craftz.dog/')
+  t.log('fixed tree:', resolved)
+  const result = toMarkdown(mdast)
+  t.log('result:', result)
+  t.is(typeof result, 'string')
+  t.is(result, '[link](#about)\n')
+})
